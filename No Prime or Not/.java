af2669f24f5c4dda1b5bc0359e7fceddd17aa8cd
SagarChopane <93799762+7CScode@users.noreply.github.com>
2022-02-01 23:11:08 +0530

    Create .java

---
+++ No Prime or Not/.java
@@ -0,0 +1,13 @@
+     public class prime {
+             public static void main(String[] args) {
+                 int a = 36;
+                 for( int i=2; i*i<=a; i++){               // i*i<=a
+                    if(i%a==0){                            // i%a==0  
+                    System.out.println("not prime");
+        }
+        
+         System.out.println("prime");
+    }
+    }
+}
+       
